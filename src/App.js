a4358380e57f617111e024895a7750ce6a89a250
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-01-23 23:09:39 -0500

    add album route and link for landing and library

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import Landing from './components/Landing.js';
 import Library from './components/Library.js';
+import album from './components/album.js';
 
 class App extends Component {
   render() {
@@ -10,12 +11,15 @@ class App extends Component {
       <div className="App">
         <header>
         <nav>
+        <Link to='/'>Landing</Link>
+             <Link to='/library'>Library</Link>
            </nav>
           <h1>Bloc Jams</h1>
         </header>
         <main>
         <Route exact path="/" component={Landing} />
            <Route path="/Library" component={Library} />
+           <Route path="/album" component={album} />
         </main>
       </div>
     );
